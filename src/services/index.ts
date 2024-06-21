a1547a9d3044f0dddd4f0901498b9711e0bb2dae
details.wes <details.wes@gmail.com>
2024-06-21 17:21:15 +0530

    refactored session handling

--- src/services/index.ts
+++ src/services/index.ts
@@ -15,4 +15,17 @@ axiosInstance.interceptors.request.use(
     }
 );
 
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem("token");
+            window.location.replace("/auth");
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default axiosInstance;
