e824f40c06c0b4a2e552bb6cd0f148cf8484c573
details.wes <details.wes@gmail.com>
2024-06-26 18:50:39 +0530

    added login test cases url

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,6 @@
+describe("Login Test", () => {
+    it("login page visible", () => {
+        // Visit the login page
+        cy.visit("http://localhost:4173/auth"); // Replace with your actual login page URL
+    });
+});
